fc01b10273b8bec6e36b6eb4cad543ad025c72c3
Brittany Hartmire <bkhartmire@gmail.com>
2019-08-20 20:59:03 +0900

    fetch weather location ip done

--- utils/api.js
+++ utils/api.js
@@ -39,57 +39,82 @@ const getIPData = async () => {
       }
     }
   );
-  const country = ipData.data.country_name;
-  const lat = ipData.data.latitude;
-  const long = ipData.data.longitude;
-  return { country, lat, long };
+  const data = ipData.data;
+  return {
+    country: data.country_name,
+    city: data.city,
+    lat: data.latitude,
+    long: data.longitude
+  };
 };
 
 const getTimeLeft = async (country, birthYear) => {
   const date = new Date();
   const dateNow = date.toISOString().split("T")[0];
-  const age = dateNow.split("-")[0] - ~~birthYear;
+  const age = ~~dateNow.split("-")[0] - ~~birthYear;
   const male = await axios.get(
     `http://54.72.28.201:80/1.0/life-expectancy/remaining/male/${country}/${dateNow}/${age}y/`
   );
   const female = await axios.get(
     `http://54.72.28.201:80/1.0/life-expectancy/remaining/female/${country}/${dateNow}/${age}y/`
   );
+
   return {
-    male: Math.round(male.data.remaining_life_expectancy),
-    female: Math.round(female.data.remaining_life_expectancy)
+    male: {
+      remaining: Math.round(male.data.remaining_life_expectancy),
+      year:
+        Math.round(male.data.remaining_life_expectancy) +
+        ~~dateNow.split("-")[0]
+    },
+    female: {
+      remaining: Math.round(female.data.remaining_life_expectancy),
+      year:
+        Math.round(female.data.remaining_life_expectancy) +
+        ~~dateNow.split("-")[0]
+    }
   };
 };
 
 const getWeather = async (lat, long, time) => {
   const weather = await axios.get(
-    `https://dark-sky.p.rapidapi.com/${lat},${long},${time}?exclude=currently,minutely,hourly,alerts,flags`
+    `https://dark-sky.p.rapidapi.com/${lat},${long},${time}?exclude=currently,minutely,hourly,alerts,flags`,
+    {
+      headers: {
+        "X-RapidAPI-Host": process.env.DARK_SKY_HOST,
+        "X-RapidAPI-Key": process.env.X_RAPID_API_Key
+      }
+    }
   );
+  const data = weather.data.daily.data.pop();
   return {
-    summary: weather.data.daily.data.summary,
-    tempHigh: weather.data.daily.data.temperatureHigh,
-    tempLow: weather.data.daily.data.temperatureLow
+    summary: data.summary,
+    tempHigh: data.temperatureHigh + " F",
+    tempLow: data.temperatureLow + " F"
   };
 };
 
 const getBirthdayData = async (year, month, day) => {
   const locationData = await getIPData();
-  // Ex: { country: 'Japan', lat: 35.6882, long: 139.7532 }
+  // Ex: { country: 'Japan', city: 'Toky', lat: 35.6882, long: 139.7532 }
 
   const timeLeft = await getTimeLeft(locationData.country, year);
-  // Ex: { male: 63, female: 70 }
+  // Ex: { male: { remaining: 63, year: 2082 }, female: { remaining: 70, year: 2089 } }
+
   const time = `${year}-${month}-${day}T12:00:00`;
   const weather = await getWeather(locationData.lat, locationData.long, time);
+  // Ex: { summary: 'Partly cloudy throughout the day.', tempHigh: '58.54 F', tempLow: '39.99 F' }
 
-  // const dummyObject = {
-  //   topSong: { title: "whatever", artist: "whoever" },
-  //   metricBirthdate: "10000",
-  //   lifeExpectancy: { male: 2060, female: 2068 },
-  //   birthdayBuddies: ["Name Namerson", "Namely Nameland"],
-  //   peopleWhoDied: ["I. M. Dead", "Died-anne Buried"]
-  // };
-  // return dummyObject;
+  const dummyObject = {
+    topSong: { title: "whatever", artist: "whoever" },
+    metricBirthdate: "10000",
+    lifeExpectancy: timeLeft,
+    location: { country: locationData.country, city: locationData.city },
+    weather,
+    birthdayBuddies: ["Name Namerson", "Namely Nameland"],
+    peopleWhoDied: ["I. M. Dead", "Died-anne Buried"]
+  };
+  return dummyObject;
 };
 
-getBirthdayData("1995");
+getBirthdayData("1995", "11", "29");
 module.exports = { getBirthdayData, getHeadlines };
