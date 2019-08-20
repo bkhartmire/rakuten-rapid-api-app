4e7e4f3c06a4a95a60eb4f3fec1e658c73edc14a
Jill <amnesiacsiblings@gmail.com>
2019-08-20 14:32:54 +0900

    dummy promise to represent fetched api data

---
+++ utils/api.js
@@ -0,0 +1,14 @@
+function getBirthdayData() {
+  apiPromiseDummy = new Promise (resolve, reject) => {
+    const dummyObject = {
+    "topSong": {"title": "whatever", "artist": "whoever"},
+    "metricBirthdate": "10000",
+    "lifeExpectancy": {"male": 2060, "female": 2068},
+    "birthdayBuddies": ["Name Namerson", "Namely Nameland"],
+    "peopleWhoDied": ["I. M. Dead", "Died-anne Buried"],
+    "headlines": [{"headline": "55 Dead in 'Yakitori Incident'", "description": "Over fifty schoolchildren have died after a collision with a yakitori food truck..."}],
+    }
+    resolve(apiPromiseDummy);
+  }
+  return apiPromiseDummy;
+}
