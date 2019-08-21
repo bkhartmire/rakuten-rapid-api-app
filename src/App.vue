d45acbd9330d4ef767c23f1996b0d3814b6c9124
Brittany Hartmire <bkhartmire@gmail.com>
2019-08-21 15:03:08 +0900

    birthday buddies and conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" src="./assets/logo.png" />
     <BirthdayFilter />
-    <GeneralResults/>
-    <Headlines/>
+    <div v-if="$store.state.showGeneralResults">
+      <GeneralResults />
+    </div>
+    <div v-if="$store.state.showHeadlinesResults">
+      <Headlines />
+    </div>
   </div>
 </template>
 
 <script>
-import BirthdayFilter from './components/BirthdayFilter.vue'
-import Headlines from './components/Headlines.vue'
-import GeneralResults from './components/GeneralResults.vue'
-
+import BirthdayFilter from "./components/BirthdayFilter.vue";
+import Headlines from "./components/Headlines.vue";
+import GeneralResults from "./components/GeneralResults.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     BirthdayFilter,
     Headlines,
     GeneralResults
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/PopCulture.vue
+++ src/components/PopCulture.vue
@@ -1,54 +1,35 @@
 <template>
   <div id="pop-culture">
     <h1>Pop Culture</h1>
-    <h2> Billboard #1 </h2>
-    <div class="billboard-artist">{{$store.state.generalResults.topSong.title}} by {{$store.state.generalResults.topSong.artist}}</div>
-    <h2> Notable Birthday Buddies </h2>
-    <div v-for="buddy in $store.state.generalResults.birthdayBuddies">
-        <div>{{buddy}}</div>
+    <h2>Billboard #1</h2>
+    <div
+      class="billboard-artist"
+    >{{$store.state.generalResults.topSong.title}} by {{$store.state.generalResults.topSong.artist}}</div>
+    <div class="birthday-buddy">
+      <h2>Notable Birthday Buddy</h2>
+      <h3>{{$store.state.generalResults.birthdayBuddy.name}}</h3>
+      <h4
+        v-if="$store.state.generalResults.birthdayBuddy.occupation"
+      >{{$store.state.generalResults.birthdayBuddy.occupation}}</h4>
+      <h4
+        v-if="$store.state.generalResults.birthdayBuddy.notable"
+      >{{$store.state.generalResults.birthdayBuddy.notable}}</h4>
+      <h4
+        v-if="$store.state.generalResults.birthdayBuddy.born && $store.state.generalResults.birthdayBuddy.died"
+      >{{$store.state.generalResults.birthdayBuddy.born}} - {{$store.state.generalResults.birthdayBuddy.died}}</h4>
     </div>
-
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'pop-culture',
-}
+  name: "pop-culture"
+};
 </script>
 
 <style>
 #pop-culture {
-    float:right;
-    width:50%;
-
+  float: right;
+  width: 50%;
 }
-
 </style>
-
-// {
-//   "topSong": {
-//     "title": "whatever",
-//     "artist": "whoever"
-//   },
-//   "metricBirthdate": "10000",
-//   "lifeExpectancy": {
-//     "male": 2060,
-//     "female": 2068
-//   },
-//   "birthdayBuddies": [
-//     "Name Namerson",
-//     "Namely Nameland"
-//   ],
-//   "peopleWhoDied": [
-//     "I. M. Dead",
-//     "Died-anne Buried"
-//   ]
-// }
-
-
-
-
-   <div class="people-who-died"/>
-      <div class="birthday-buddies"/>
