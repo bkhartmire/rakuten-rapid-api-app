02c2661e350886e873521269fca5d9ef3be5a263
Brittany Hartmire <bkhartmire@gmail.com>
2019-08-21 17:01:29 +0900

    remove weather and ip fetch

--- src/components/PopCulture.vue
+++ src/components/PopCulture.vue
@@ -1,64 +1,55 @@
 <template>
   <div id="pop-culture">
-  <BJumbotron class="jumboPop" header="Pop Culture">
-  <BListGroup>
-  <BListGroupItem   class="flex-column align-items-start">
-    <h2> Billboard #1 </h2>
-    <!-- <div class="billboard-artist text-muted">{{$store.state.generalResults.topSong.title}} by {{$store.state.generalResults.topSong.artist}}</div> !-->
-    </BListGroupItem>
-      <BListGroupItem   class="flex-column align-items-start">
-    <div class="birthday-buddy">
-      <h2>Notable Birthday Buddy</h2>
-      <h3>{{$store.state.generalResults.birthdayBuddy.name}}</h3>
-      <h4
-        v-if="$store.state.generalResults.birthdayBuddy.occupation"
-      >{{$store.state.generalResults.birthdayBuddy.occupation}}</h4>
-      <h4
-        v-if="$store.state.generalResults.birthdayBuddy.notable"
-      >{{$store.state.generalResults.birthdayBuddy.notable}}</h4>
-      <h4
-        v-if="$store.state.generalResults.birthdayBuddy.born && $store.state.generalResults.birthdayBuddy.died"
-      >{{$store.state.generalResults.birthdayBuddy.born}} - {{$store.state.generalResults.birthdayBuddy.died}}</h4>
-    </div>
-
-
-    </BListGroupItem>
-
-    </BListGroupItem>
-    </BListGroup>
+    <BJumbotron class="jumboPop" header="Pop Culture">
+      <BListGroup>
+        <BListGroupItem class="flex-column align-items-start">
+          <h2>Billboard #1</h2>
+          <div
+            class="billboard-artist text-muted"
+          >{{$store.state.generalResults.topSong.title}} by {{$store.state.generalResults.topSong.artist}}</div>
+        </BListGroupItem>
+        <BListGroupItem class="flex-column align-items-start">
+          <div class="birthday-buddy">
+            <h2>Notable Birthday Buddy</h2>
+            <h3>{{$store.state.generalResults.birthdayBuddy.name}}</h3>
+            <h4
+              v-if="$store.state.generalResults.birthdayBuddy.occupation"
+            >{{$store.state.generalResults.birthdayBuddy.occupation}}</h4>
+            <h4
+              v-if="$store.state.generalResults.birthdayBuddy.notable"
+            >{{$store.state.generalResults.birthdayBuddy.notable}}</h4>
+            <h4
+              v-if="$store.state.generalResults.birthdayBuddy.born && $store.state.generalResults.birthdayBuddy.died"
+            >{{$store.state.generalResults.birthdayBuddy.born}} - {{$store.state.generalResults.birthdayBuddy.died}}</h4>
+          </div>
+        </BListGroupItem>
+      </BListGroup>
     </BJumbotron>
-    
-
-
-  
   </div>
 </template>
 
 <script>
-import { BJumbotron, BListGroup, BListGroupItem} from 'bootstrap-vue'
+import { BJumbotron, BListGroup, BListGroupItem } from "bootstrap-vue";
 
 export default {
-  name: 'pop-culture',
-  component:{
-      BListGroup,
-      BListGroupItem,
+  name: "pop-culture",
+  component: {
+    BListGroup,
+    BListGroupItem
   }
-}
+};
 </script>
 
 <style>
 #pop-culture {
-    display: inline-block;
-    width: 50%;
-    align-items: right;
-    vertical-align: top;
+  display: inline-block;
+  width: 50%;
+  align-items: right;
+  vertical-align: top;
 }
 .jumboPop {
-    padding:0;
-    
+  padding: 0;
 }
-
-
 </style>
 
 
